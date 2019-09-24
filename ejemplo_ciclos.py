f3307d9e33c947c9d697806a4788591cd26e074b
Beyond-sama <reyko.mikami@gmail.com>
2019-09-24 10:37:15 -0300

    agregamos un ej de uso de ciclos

---
+++ ejemplo_ciclos.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#guardar como ejemplo_ciclos.py
+#notese los detalles pues python la indentacion es importante
+
+i=10
+while i > 0 :
+    print(i)
+    i = i - 1
+
+#ahora un for
+regiones=["arica", "tarapara", "antofagasta", "atacama"]
+for r in regiones:
+    print(r)
