ebf48478a6b239331a250ad75ebe4f7c6c13dc0b
Beyond-sama <reyko.mikami@gmail.com>
2019-09-24 10:17:17 -0300

    agregamos el hola mundo

---
+++ hola_mundo.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+#linea de arriba para q corra en linux
+#en windows no es necesario pero yo lo suelo poner igual
+
+#guardar en la carpeta del proyecto con el nombre
+#hola_mundo.py
+
+print('Hola Mundo')
